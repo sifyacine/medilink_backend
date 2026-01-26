c2ba778acd76620d4db43d4894658b7226d7da60
zak kherfi <a.zakaria@kherfi.com>
2026-01-26 12:29:05 +0100

    update the base to use the ,env and update the gitignore

--- core/settings/base.py
+++ core/settings/base.py
@@ -65,15 +65,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
 }
 
 
