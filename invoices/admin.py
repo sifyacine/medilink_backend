501cba5f12c21d4e9380801f0377b41947436ab1
sif yacine <ycn585@gmail.com>
2026-03-04 13:55:10 +0100

    feat: Allow superusers to delete invoice activities in admin interface

--- invoices/admin.py
+++ invoices/admin.py
@@ -255,4 +255,4 @@ class InvoiceActivityAdmin(admin.ModelAdmin):
         return False
     
     def has_delete_permission(self, request, obj=None):
-        return False
+        return request.user.is_superuser
